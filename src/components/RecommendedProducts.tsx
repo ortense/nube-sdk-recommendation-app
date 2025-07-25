deeb3a29b349858abd50f7e152a4f4d4d3fb8622
Marcus Ortense <marcus.ortense@outlook.com>
2025-07-24 23:50:38 -0300

    feat: horizontal scroll

--- src/components/RecommendedProducts.tsx
+++ src/components/RecommendedProducts.tsx
@@ -1,22 +1,33 @@
-import { Row } from "@tiendanube/nube-sdk-jsx";
+import { Column, Row, Text } from "@tiendanube/nube-sdk-jsx";
 import { addProductToCart, updateRecommendation, useProducts } from "../state";
 import { ProductCard } from "./ProductCard";
+import { styled } from "@tiendanube/nube-sdk-ui";
+
+const ProductRow = styled(Row)`
+	max-width: 100%;
+	overflow-x: auto;
+	padding: 2em;
+	gap: 10px;
+`;
 
 export function RecommendedProducts() {
 	const products = useProducts();
 
 	return (
-		<Row gap="10px" padding="2em">
-			{products.map((props) => (
-				// biome-ignore lint/correctness/useJsxKeyInIterable: temporary fix
-				<ProductCard
-					{...props}
-					onAddToCart={addProductToCart}
-					onSelectVariant={(id, variant) =>
-						updateRecommendation(id, { selectedVariant: Number(variant) })
-					}
-				/>
-			))}
-		</Row>
+		<Column>
+			<Text heading={2}>Compre junto!!</Text>
+			<ProductRow>
+				{products.map((props) => (
+					// biome-ignore lint/correctness/useJsxKeyInIterable: temporary fix
+					<ProductCard
+						{...props}
+						onAddToCart={addProductToCart}
+						onSelectVariant={(id, variant) =>
+							updateRecommendation(id, { selectedVariant: Number(variant) })
+						}
+					/>
+				))}
+			</ProductRow>
+		</Column>
 	);
 }
